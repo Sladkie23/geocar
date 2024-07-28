9e7dc23b4eae2e23b7208f4061bd419fc3cedca2
Sladkie23 <mentyjopa@mail.ru>
2024-07-28 17:58:48 +0300

    Полное добавление машин через админку

--- admin/addcar.php
+++ admin/addcar.php
@@ -4,13 +4,18 @@ $conn = mysqli_connect('MySQL-8.2', 'root', '', 'car_base');
 $data = json_decode(file_get_contents('php://input'), true);
 
 
+$photo1 = $data['photo1'];
+$photo2 = $data['photo2'];
+$photo3 = $data['photo3'];
+$photo4 = $data['photo4'];
 $name = $data['name'];
-$date = $data['year'];
+$year = $data['year'];
+$engine = $data['engine'];
 $price = $data['price'];
 
 
-$query = "INSERT INTO car_list(name, date, price)
-          VALUES('$name', '$date', '$price')";
+$query = "INSERT INTO car_list(photo1, photo2, photo3, photo4, name, year, engine, price)
+          VALUES('$photo1', '$photo2', '$photo3', '$photo4', '$name', '$year', '$engine', '$price')";
 
 $result = mysqli_query($conn, $query);
 
--- admin/admin.js
+++ admin/admin.js
@@ -1,34 +1,31 @@
 "use strict"
 //переменные
-
-const imageInput = document.getElementById("imageInput")
-const carImage = document.getElementById("carImage")
 const form = document.getElementById('form')
-
-//отображение картинки после загрузки
-
-imageInput.addEventListener("input",(event)=> {
-    let reader = new FileReader();
-    reader.onload = function() {
-        carImage.src = reader.result;
-        carImage.style.display = 'block';
-    };
-    reader.readAsDataURL(event.target.files[0]);
-})
-
-
-
-
 //  Отправка данных на сервер
 
 form.addEventListener('submit',(event)=>{
     event.preventDefault()
     const data = {
-        name: document.getElementById('name').value,
-        year: document.getElementById('date').value,
-        price: document.getElementById('price').value,
+        photo1: document.getElementById("photo1").value,
+        photo2: document.getElementById("photo2").value,
+        photo3: document.getElementById("photo3").value,
+        photo4: document.getElementById("photo4").value,
+        name: document.getElementById("name").value,
+        year: document.getElementById("year").value,
+        engine: document.getElementById("engine").value,
+        price: document.getElementById("price").value
     }
-    return sendData('./addcar.php', data)
+    document.getElementById("photo1").value = ""
+    document.getElementById("photo2").value = ""
+    document.getElementById("photo3").value = ""
+    document.getElementById("photo4").value = ""
+    document.getElementById("name").value = ""
+    document.getElementById("year").value = ""
+    document.getElementById("engine").value = ""
+    document.getElementById("price").value = ""
+    alert("Car added!")
+    
+    return sendData('/addcar.php', data)
 })
 
 
