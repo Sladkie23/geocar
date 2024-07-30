c782680ee45ced43d5508cabca13a20df0e3c98d
Sladkie23 <mentyjopa@mail.ru>
2024-07-30 14:13:40 +0300

    READY TO HOST

--- public/lang.js
+++ public/lang.js
@@ -1,29 +1,94 @@
-const langSelect = document.querySelector(".lang__select")
-const lang = document.querySelector(".lang")
+const langSelect = document.querySelectorAll(".lang__select")
+const lang = document.querySelectorAll(".lang")
 const langPos = document.querySelectorAll(".lang__pos")
 
-lang.addEventListener("click", ()=> {
-    if (langSelect.getAttribute("class") == "lang__select") {
-        langSelect.setAttribute("class", "lang__select lang__select__animation")
-        setTimeout(()=> {
+for (let i = 0; i < lang.length; i++) {
+    lang[i].addEventListener("click", ()=> {
+        if (langSelect[i].getAttribute("class") == "lang__select") {
+            langSelect[i].setAttribute("class", "lang__select lang__select__animation")
+            setTimeout(()=> {
+                for (let i = 0; i < langPos.length; i++) {
+                    langPos[i].setAttribute("class", "lang__pos lang__pos__animation")
+                }
+            }, 450)
+        } else {
+            langSelect[i].setAttribute("class", "lang__select")
             for (let i = 0; i < langPos.length; i++) {
-                langPos[i].setAttribute("class", "lang__pos lang__pos__animation")
+                langPos[i].setAttribute("class", "lang__pos")
             }
-        }, 450)
-    } else {
-        langSelect.setAttribute("class", "lang__select")
-        for (let i = 0; i < langPos.length; i++) {
-            langPos[i].setAttribute("class", "lang__pos")
         }
-    }
-})
+    })
+}
 
-document.addEventListener("mouseup", (event)=> {
-    if (event.target.textContent != "LANGUAGE") {
-        langSelect.setAttribute("class", "lang__select")
-        for (let i = 0; i < langPos.length; i++) {
-            langPos[i].setAttribute("class", "lang__pos")
+
+const HOME = document.getElementById("HOME")
+const OURCARS = document.getElementById("OURCARS")
+const CONTACTS = document.getElementById("CONTACTS")
+const LANGUAGE = document.getElementById("LANGUAGE")
+const RENTCARINTBILISI = document.getElementById("RENTCARINTBILISI")
+const FASTANDEASYBOOKING = document.getElementById("FASTANDEASYBOOKING")
+const noticeP1 = document.getElementById("noticeP1")
+const MANYPICKUPLOCATION = document.getElementById("MANYPICKUPLOCATION")
+const noticeP2 = document.getElementById("noticeP2")
+const SAFETRAVELS = document.getElementById("SAFETRAVELS")
+const noticeP3 = document.getElementById("noticeP3")
+const CARSFORRENT = document.getElementById("CARSFORRENT")
+const CALLTODAY = document.getElementById("CALLTODAY")
+const OURLOCATION = document.getElementById("OURLOCATION")
+const HOURS = document.getElementById("HOURS")
+
+
+for (let i = 0; i < langPos.length; i++) {
+    langPos[i].addEventListener("click", ()=> {
+        if(langPos[i].getAttribute("id")== "eng") {
+            HOME.innerHTML = `<img src="/materials/images/Head_HomeIcon.png">HOME`
+            OURCARS.innerHTML = `<img src="/materials/images/Head_CarIcon.png"><a href="#mclc">OUR CARS</a>`
+            CONTACTS.innerHTML = `<img src="/materials/images/Head_ContIocn.png"><a href="#cbc">CONTACTS</a>`
+            LANGUAGE.innerHTML = `LANGUAGE<img src="/materials/images/Head_LangIcon.png">`
+            RENTCARINTBILISI.innerHTML = `Rent Car in<br> Tbilisi`
+            FASTANDEASYBOOKING.innerHTML = `Fast & Easy Booking`
+            noticeP1.innerHTML = `Effortless Reservations Await! Skip the clicks and dial into simplicity. Our Fast & Easy Booking ensures instant access to your dream ride. Reach us with a quick call, and let the journey begin. Swift reservations, personalized service – because your travel plans deserve a human touch. Connect with us now for a seamless road ahead!`
+            MANYPICKUPLOCATION.innerHTML = `Many Pickup Locations`
+            noticeP2.innerHTML = `Diverse Destinations, One Stop! Explore the freedom of choice with our Many Pickup Locations. From city centers to airports, we’ve got you covered. No matter where you are, your journey begins at your convenience. Discover the ease of multiple options – because the road to your adventure starts where you are. Find your ideal pickup point and let the exploration unfold!`
+            SAFETRAVELS.innerHTML = `Safe Travels`
+            noticeP3.innerHTML = `Family adventures made secure! Our commitment to safety extends to the youngest members of your crew. Introducing our Baby Seat Service – ensuring your little explorers ride in comfort and security. Choose peace of mind for every journey, knowing that we prioritize the safety of your precious passengers. Elevate your family travel experience with our hassle-free Baby Seat Service.`
+            CARSFORRENT.innerHTML = `CARS FOR RENT`
+            CALLTODAY.innerHTML = `Call Today For Booking Your Next Ride`
+            OURLOCATION.innerHTML = `Our Location<img src="/materials/images/Loc_Gps.png">`
+            HOURS.innerHTML = `<b>Hours:</b><br><br>24/7 whole week`
+        } else if (langPos[i].getAttribute("id") == "geo") {
+            HOME.innerHTML = `<img src="/materials/images/Head_HomeIcon.png">მთავარი`
+            OURCARS.innerHTML = `<img src="/materials/images/Head_CarIcon.png"><a href="#mclc">კატალოგი</a>`
+            CONTACTS.innerHTML = `<img src="/materials/images/Head_ContIocn.png"><a href="#cbc">კონტაქტები</a>`
+            LANGUAGE.innerHTML = `ᲔᲜᲐ<img src="/materials/images/Head_LangIcon.png">`
+            RENTCARINTBILISI.innerHTML = `ქირავდება მანქანა <br>თბილისში`
+            FASTANDEASYBOOKING.innerHTML = `მარტივი დაჯავშნა`
+            noticeP1.innerHTML = `უპრობლემოდ დაჯავშნა გელოდებათ! გამოტოვეთ დაწკაპუნებები და აკრიფეთ სიმარტივე`
+            MANYPICKUPLOCATION.innerHTML = `ბევრი პიკაპის ადგილი`
+            noticeP2.innerHTML = `მრავალფეროვანი მიმართულებები, ერთი გაჩერება! გამოიკვლიეთ არჩევანის თავისუფლება ჩვენი მრავალი პიკაპის ლოკაციებით`
+            SAFETRAVELS.innerHTML = `უსაფრთხო მოგზაურობა`
+            noticeP3.innerHTML = `საოჯახო თავგადასავლები დაცულია! ჩვენი ვალდებულება უსაფრთხოების მიმართ ვრცელდება თქვენი ეკიპაჟის ყველაზე ახალგაზრდა წევრებზე`
+            CARSFORRENT.innerHTML = `ქირავდება მანქანები`
+            CALLTODAY.innerHTML = `დარეკეთ დღეს თქვენი შემდეგი მოგზაურობის დასაჯავშნად`
+            OURLOCATION.innerHTML = `მდებარეობა<img src="/materials/images/Loc_Gps.png">`
+            HOURS.innerHTML = `<b>საათები:</b><br><br>24/7 მთელი კვირა`
+        } else if (langPos[i].getAttribute("id") == "rus") {
+            HOME.innerHTML = `<img src="/materials/images/Head_HomeIcon.png">ГЛАВНАЯ`
+            OURCARS.innerHTML = `<img src="/materials/images/Head_CarIcon.png"><a href="#mclc">МАШИНЫ</a>`
+            CONTACTS.innerHTML = `<img src="/materials/images/Head_ContIocn.png"><a href="#cbc">КОНТАКТЫ</a>`
+            LANGUAGE.innerHTML = `ЯЗЫК<img src="/materials/images/Head_LangIcon.png">`
+            RENTCARINTBILISI.innerHTML = `Аренда машин<br>в Тбилиси`
+            FASTANDEASYBOOKING.innerHTML = `Быстро и легко!`
+            noticeP1.innerHTML = `Мы стремились упростить процесс аренды. Всё проще, чем кажется. Свяжитесь с нами в один звонок или сообщение и ваше путешествие начнется.`
+            MANYPICKUPLOCATION.innerHTML = `Удобное расположение`
+            noticeP2.innerHTML = `Нам было крайне важно, чтобы вам было удобно арендовать авто, поэтому удобный пункт выдачи авто позволяет добраться до вашей машины максимально быстро из любой точки`
+            SAFETRAVELS.innerHTML = `Безопасные путешествия`
+            noticeP3.innerHTML = `Семейные приключения в безопасности! Наша приверженность безопасности распространяется и на самых юных членов вашего экипажа. Представляем нашу услугу детского сиденья, обеспечивающую комфорт и безопасность ваших маленьких исследователей`
+            CARSFORRENT.innerHTML = `МАШИНЫ ДЛЯ АРЕНДЫ`
+            CALLTODAY.innerHTML = `Звоните сегодня, чтобы забронировать ваше следующее путешествие`
+            OURLOCATION.innerHTML = `Мы на карте<img src="/materials/images/Loc_Gps.png">`
+            HOURS.innerHTML = `<b>Часы:</b><br><br>24/7 Всю неделю`
         }
-    }
-})
+    })
+}  
 
--- public/main.js
+++ public/main.js
@@ -15,82 +15,120 @@ const getResource = async(url) => {
 
 ////////////////////////Отображение машин из БД в HTML///////////////////////////
 
+
+
 const carList = document.querySelector(".car__list")
 
-async function CarsLoad() {
-    return await getResource(url).then(
-        (data)=>{
-            data.forEach(element => {
-                let carCard = document.createElement("div")
-                carList.insertAdjacentElement("afterbegin", carCard)
-                carCard.setAttribute("class", "car__card")
-                carCard.setAttribute("id", element[0])
-                carCard.setAttribute("photo1", element[1])
-                carCard.setAttribute("photo2", element[2])
-                carCard.setAttribute("photo3", element[3])
-                carCard.setAttribute("photo4", element[4])
+document.addEventListener("DOMContentLoaded", () => {
+   async function CarsLoad() {
+        return await getResource(url).then(
+            (data)=>{
+                data.forEach(element => {
+                    let carCard = document.createElement("div")
+                    carList.insertAdjacentElement("afterbegin", carCard)
+                    carCard.setAttribute("class", "car__card")
+                    carCard.setAttribute("id", element[0])
+        
+        
+                    let rentButton = document.createElement("div")
+                    carCard.insertAdjacentElement("afterbegin", rentButton)
+                    rentButton.setAttribute("class", "rent__button")
+                    rentButton.setAttribute("photo1", element[1])
+                    rentButton.setAttribute("photo2", element[2])
+                    rentButton.setAttribute("photo3", element[3])
+                    rentButton.setAttribute("photo4", element[4])
+                    rentButton.setAttribute("year", element[6])
+                    rentButton.setAttribute("engine", element[7])
+                    rentButton.setAttribute("price", element[8])
+                    
+                    let rentbuttonP = document.createElement("p")
+                    rentbuttonP.textContent = "RENT"
+                    let rentButtonArrow = document.createElement("img")
+                    rentButtonArrow.setAttribute("src", "/materials/images/Arrow_icon 1.png")
+                    rentButton.insertAdjacentElement("afterbegin", rentButtonArrow)
+                    rentButton.insertAdjacentElement("afterbegin", rentbuttonP)
+                
+                    carInfo = document.createElement("div")
+                    carCard.insertAdjacentElement("afterbegin", carInfo)
+                    carInfo.setAttribute("class", "car__info")
+                    let p1 = document.createElement("p")
+                    p1.textContent = element[5]
+                    let p2 = document.createElement("p")
+                    p2.textContent = element[6]
+                    let p3 = document.createElement("p")
+                    p3.textContent = element[7]
+                    carInfo.insertAdjacentElement("afterbegin", p3)
+                    carInfo.insertAdjacentElement("afterbegin", p2)
+                    carInfo.insertAdjacentElement("afterbegin", p1)
+        
+                    let carImage = document.createElement("div")
+                    carCard.insertAdjacentElement("afterbegin", carImage)
+                    carImage.setAttribute("class", "car__image")
+                    let carImageTag = document.createElement("img")
+                    carImageTag.setAttribute("src", element[1])
+                    carImage.insertAdjacentElement("afterbegin", carImageTag)
     
-    
-                let rentButton = document.createElement("div")
-                carCard.insertAdjacentElement("afterbegin", rentButton)
-                rentButton.setAttribute("class", "rent__button")
-                let rentbuttonP = document.createElement("p")
-                rentbuttonP.textContent = "RENT"
-                let rentButtonArrow = document.createElement("img")
-                rentButtonArrow.setAttribute("src", "/materials/images/Arrow_icon 1.png")
-                rentButton.insertAdjacentElement("afterbegin", rentButtonArrow)
-                rentButton.insertAdjacentElement("afterbegin", rentbuttonP)
-            
-                carInfo = document.createElement("div")
-                carCard.insertAdjacentElement("afterbegin", carInfo)
-                carInfo.setAttribute("class", "car__info")
-                let p1 = document.createElement("p")
-                p1.textContent = element[5]
-                let p2 = document.createElement("p")
-                p2.textContent = element[6]
-                let p3 = document.createElement("p")
-                p3.textContent = element[7]
-                carInfo.insertAdjacentElement("afterbegin", p3)
-                carInfo.insertAdjacentElement("afterbegin", p2)
-                carInfo.insertAdjacentElement("afterbegin", p1)
-    
-                let carImage = document.createElement("div")
-                carCard.insertAdjacentElement("afterbegin", carImage)
-                carImage.setAttribute("class", "car__image")
-                let carImageTag = document.createElement("img")
-                carImageTag.setAttribute("src", element[1])
-                carImage.insertAdjacentElement("afterbegin", carImageTag)
+                    const rentButtonView = document.querySelectorAll(".rent__button")
+                    const rentPopUpBg = document.querySelector(".rent__pop__up__bg")
+                    const rentPopUpBgCont = document.querySelector(".rent__pop__up__bg__container")
+                    const cross = document.querySelector(".cross")
 
-                const rentButtonView = document.querySelectorAll(".rent__button")
-                const rentPopUpBg = document.querySelector(".rent__pop__up__bg")
-                const rentPopUpBgCont = document.querySelector(".rent__pop__up__bg__container")
-                const cross = document.querySelector(".cross")
+                    for (let i = 0; i < rentButtonView.length; i++) {
+                        let cardButton = rentButtonView[i];
+                        cardButton.addEventListener("click", ()=>{
+                            rentPopUpBg.setAttribute("class", "rent__pop__up__bg")
+                            rentPopUpBgCont.setAttribute("class", "rent__pop__up__bg__container")
+                        }) 
+                    }
+        
+                    cross.addEventListener("click", ()=>{
+                        rentPopUpBg.setAttribute("class", "rent__pop__up__bg no-vision")
+                        rentPopUpBgCont.setAttribute("class", "rent__pop__up__bg__container no-vision")
+                    })
+                });
+                let buttonsRent = document.querySelectorAll(".rent__button")
                 const arrows = document.querySelectorAll(".arrows img")
 
-            });
-            for (let i = 0; i < rentButtonView.length; i++) {
-                let cardButton = rentButtonView[i];
-                cardButton.addEventListener("click", ()=>{
-                    rentPopUpBg.setAttribute("class", "rent__pop__up__bg")
-                    rentPopUpBgCont.setAttribute("class", "rent__pop__up__bg__container")
-                }) 
+                for (let i = 0; i < buttonsRent.length; i++) {
+                    buttonsRent[i].addEventListener("click", ()=> {
+                        let popUpYear = document.getElementById("pop-up-year")
+                        let popUpEngine = document.getElementById("pop-up-engine")
+                        let popUpPrice = document.querySelector(".price p")
+                        let rpuContainerImg = document.querySelector(".rpu__container img")
+                        popUpYear.innerHTML = `<b>Year of release</b><br>${buttonsRent[i].getAttribute("year")}`
+                        popUpEngine.innerHTML = `<b>Engine:</b><br>${buttonsRent[i].getAttribute("engine")}`
+                        popUpPrice.innerHTML = `<b>${buttonsRent[i].getAttribute("price")}</b> GEL`
+                        rpuContainerImg.setAttribute("src", buttonsRent[i].getAttribute("photo1"))
+
+                        const cross = document.querySelector(".cross")
+                        let photoCount = 1
+                        cross.addEventListener("click",()=>{
+                            photoCount = 1
+                        })
+
+                        arrows[0].addEventListener("click", ()=>{
+                            if (rpuContainerImg.getAttribute("src") != buttonsRent[i].getAttribute("photo1") && photoCount > 1) {
+                                rpuContainerImg.setAttribute("src", buttonsRent[i].getAttribute(`photo${photoCount - 1}`))
+                                photoCount = photoCount - 1
+                                console.log(photoCount)
+                            }
+                        })
+                        arrows[1].addEventListener("click", ()=>{
+                            if (rpuContainerImg.getAttribute("src") != buttonsRent[i].getAttribute("photo4") && photoCount < 4) {
+                                rpuContainerImg.setAttribute("src", buttonsRent[i].getAttribute(`photo${photoCount + 1}`))
+                                photoCount = photoCount + 1
+                                console.log(photoCount)
+                            }
+                        })
+                    })
+                    
+                }
             }
+        )
+    }
+    CarsLoad()
+})
 
-            cross.addEventListener("click", ()=>{
-                rentPopUpBg.setAttribute("class", "rent__pop__up__bg no-vision")
-                rentPopUpBgCont.setAttribute("class", "rent__pop__up__bg__container no-vision")
-            })
-
-            arrows[0].addEventListener("click", ()=>{
-                console.log("left")
-            })
-            arrows[1].addEventListener("click", ()=>{
-                console.log("right")
-            })
-        }
-    )
-}
-CarsLoad()
 
 
 
